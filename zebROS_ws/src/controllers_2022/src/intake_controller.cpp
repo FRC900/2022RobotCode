1e1df86f485d51ef4733ae59de5e235ce5c6b93e
FRC900 2020.1 VM Image <pranavnarala@gmail.com>
2022-01-20 16:30:05 -0800

    Formatting the 2022 intake controller file.

--- zebROS_ws/src/controllers_2022/src/intake_controller.cpp
+++ zebROS_ws/src/controllers_2022/src/intake_controller.cpp
@@ -1,13 +1,34 @@
-#include "controllers_2022/intake_controller.h"
+#pragma once
+
+#include <ros/ros.h>
+#include <realtime_tools/realtime_buffer.h> //code for real-time buffer - stop multple things writing to same variable at same time
+
+//controller interfaces
+#include <controller_interface/multi_interface_controller.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <talon_controllers/talon_controller_interface.h>
+
+
+#include <controllers_2022_msgs/IntakeArmSrv.h>
+#include <controllers_2022_msgs/IntakeRollerSrv.h>
+#include <std_srvs/SetBool.h>
 #include <pluginlib/class_list_macros.h> //to compile as a controller
 
 namespace intake_controller
 {
-    bool IntakeController::init(hardware_interface::RobotHW *hw,
-                                     ros::NodeHandle                 &/*root_nh*/,
-                                     ros::NodeHandle                 &controller_nh)
+class IntakeController : public controller_interface::MultiInterfaceController<hardware_interface::PositionJointInterface, hardware_interface::TalonCommandInterface>
+{
+
+public:
+    IntakeController()
     {
-	hardware_interface::TalonCommandInterface *const talon_command_iface = hw->get<hardware_interface::TalonCommandInterface>();
+    }
+
+    bool IntakeController::init(hardware_interface::RobotHW *hw,
+                                ros::NodeHandle                 &/*root_nh*/,
+                                ros::NodeHandle                 &controller_nh)
+    {
+        hardware_interface::TalonCommandInterface *const talon_command_iface = hw->get<hardware_interface::TalonCommandInterface>();
         hardware_interface::PositionJointInterface *const pos_joint_iface = hw->get<hardware_interface::PositionJointInterface>();
 
         //Initialize intake piston joint
@@ -27,53 +48,66 @@ namespace intake_controller
             ROS_ERROR("Cannot initialize intake_joint!");
             return false;
         }
-	else
-	{
-		ROS_INFO("Initialized intake joint");
-	}
+        else
+        {
+            ROS_INFO("Initialized intake joint");
+        }
 
         //Initialize your ROS server
         intake_arm_service_ = controller_nh.advertiseService("intake_arm_command", &IntakeController::cmdServiceArm, this);
         intake_roller_service_ = controller_nh.advertiseService("intake_roller_command", &IntakeController::cmdServiceRoller, this);
-		intake_disable_service_ = controller_nh.advertiseService("intake_disable", &IntakeController::disableIntakeCallback, this);
+        intake_disable_service_ = controller_nh.advertiseService("intake_disable", &IntakeController::disableIntakeCallback, this);
 
-		return true;
+        return true;
     }
 
     void IntakeController::starting(const ros::Time &/*time*/) {
         //give command buffer(s) an initial value
-		arm_extend_cmd_buffer_.writeFromNonRT(true);
-		percent_out_cmd_buffer_.writeFromNonRT(0.0);
+        arm_extend_cmd_buffer_.writeFromNonRT(true);
+        percent_out_cmd_buffer_.writeFromNonRT(0.0);
 
-		forward_disabled_.writeFromNonRT(false);
+        forward_disabled_.writeFromNonRT(false);
     }
 
     void IntakeController::update(const ros::Time &/*time*/, const ros::Duration &/*period*/) {
         //grab value from command buffer(s)
         const bool arm_extend_cmd = *(arm_extend_cmd_buffer_.readFromRT());
-		double arm_extend_double;
-		if(arm_extend_cmd == true){
-			arm_extend_double = 0.0;
-		}
-		else {
-			arm_extend_double = 1.0;
-		}
+        double arm_extend_double;
+        if(arm_extend_cmd == true) {
+            arm_extend_double = 0.0;
+        }
+        else {
+            arm_extend_double = 1.0;
+        }
 
-		//if moving forwards was disabled by the indexer server, don't allow forward movement
-		double percent_out_cmd = *percent_out_cmd_buffer_.readFromRT();
-		/*if(*forward_disabled_.readFromRT() && percent_out_cmd > 0)
-		{
-			percent_out_cmd = 0.0;
-		}*/
+        //if moving forwards was disabled by the indexer server, don't allow forward movement
+        double percent_out_cmd = *percent_out_cmd_buffer_.readFromRT();
+        /*if(*forward_disabled_.readFromRT() && percent_out_cmd > 0)
+          {
+               percent_out_cmd = 0.0;
+             }*/
 
-		intake_joint_.setCommand(percent_out_cmd);
-		intake_arm_joint_.setCommand(arm_extend_double);
+        intake_joint_.setCommand(percent_out_cmd);
+        intake_arm_joint_.setCommand(arm_extend_double);
     }
 
     void IntakeController::stopping(const ros::Time &/*time*/) {
     }
 
-	bool IntakeController::cmdServiceArm(controllers_2022_msgs::IntakeArmSrv::Request &req, controllers_2022_msgs::IntakeArmSrv::Response &/*response*/) {
+private:
+
+    talon_controllers::TalonPercentOutputControllerInterface intake_joint_;//intake for intake motor
+    hardware_interface::JointHandle intake_arm_joint_;//interface for intake arm solenoid
+    realtime_tools::RealtimeBuffer<bool> arm_extend_cmd_buffer_;
+    realtime_tools::RealtimeBuffer<double> percent_out_cmd_buffer_;
+    ros::ServiceServer intake_arm_service_;
+    ros::ServiceServer intake_roller_service_;
+
+    ros::ServiceServer intake_disable_service_;
+    realtime_tools::RealtimeBuffer<bool> forward_disabled_; //set to true by the indexer server when it's finishing up properly storing a ball, to ensure the proper gap
+
+
+    bool IntakeController::cmdServiceArm(controllers_2022_msgs::IntakeArmSrv::Request &req, controllers_2022_msgs::IntakeArmSrv::Response &/*response*/) {
         if(isRunning())
         {
             //assign request value to command buffer(s)
@@ -87,7 +121,7 @@ namespace intake_controller
         return true;
     }
 
-	bool IntakeController::cmdServiceRoller(controllers_2022_msgs::IntakeRollerSrv::Request &req, controllers_2022_msgs::IntakeRollerSrv::Response &/*response*/) {
+    bool IntakeController::cmdServiceRoller(controllers_2022_msgs::IntakeRollerSrv::Request &req, controllers_2022_msgs::IntakeRollerSrv::Response &/*response*/) {
         if(isRunning())
         {
             //assign request value to command buffer(s)
@@ -101,7 +135,7 @@ namespace intake_controller
         return true;
     }
 
-	bool IntakeController::disableIntakeCallback(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &) {
+    bool IntakeController::disableIntakeCallback(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &) {
         if(isRunning())
         {
             forward_disabled_.writeFromNonRT(req.data);
@@ -114,7 +148,7 @@ namespace intake_controller
         return true;
     }
 
-
+}; //class
 }//namespace
 
 //DON'T FORGET TO EXPORT THE CLASS SO CONTROLLER_MANAGER RECOGNIZES THIS AS A TYPE
