a0524a2490e8e3d43ade02f23fda2bc5b631b4a5
BenBean18 <beakerthespeaker@gmail.com>
2022-01-21 19:27:06 -0500

    Switched to using pbtxt map

--- zebROS_ws/src/pf_localization/src/fake_goal_detect.cpp
+++ zebROS_ws/src/pf_localization/src/fake_goal_detect.cpp
@@ -10,17 +10,42 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
 
+#include <map>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <fstream>
+
+std::map<int, std::string> parsePBTXT(std::string filename) {
+	std::map<int, std::string> pbtxtMap;
+	std::regex pbtxtRegex("id:[^\\d]*(\\d+)[\\w\\d\\s]+name:.*['\"](.+)['\"]"); // this feels like an evil thing to do, but whatever
+	// regexr.com/6dql5
+	std::ifstream t(filename);
+	std::stringstream fileContents;
+	fileContents << t.rdbuf();
+	std::smatch res;
+	std::string str = fileContents.str();
+
+	std::string::const_iterator searchStart(str.cbegin());
+	while (std::regex_search(searchStart, str.cend(), res, pbtxtRegex))
+	{
+		pbtxtMap[std::stoi(res[1])] = res[2];
+		searchStart = res.suffix().first;
+	}
+	return pbtxtMap;
+}
+
 class FakeGoalDetection
 {
 	public:
-		FakeGoalDetection(ros::NodeHandle &n, ros::NodeHandle &nh_params)
+		FakeGoalDetection(ros::NodeHandle &n, const std::map<int, std::string> &objMap)
 			: rd_{}
 			, gen_{rd_()}
 			, covariance_(0.0004)
 			, sub_(n.subscribe("base_marker_detection", 2, &FakeGoalDetection::cmdVelCallback, this))
 			, pub_(n.advertise<field_obj::Detection>("goal_detect_msg", 2))
 			, pubd_(n.advertise<field_obj::Detection>("/tf_object_detection/object_detection_world", 2))
-			, nh_params_(nh_params)
+			, objMap_(objMap)
 
 		{
 			n.param("covariance", covariance_, covariance_);
@@ -36,11 +61,7 @@ class FakeGoalDetection
 			{
 				if (msgIn->markers[i].ids[0] == -1) // stage publishes odom as marker -1
 					continue;                       // ignore it here
-				if (nh_params_.hasParam("id"+std::to_string(msgIn->markers[i].ids[0]))) {
-					// that's in the config file!
-					std::string id;
-					nh_params_.getParam("id"+std::to_string(msgIn->markers[i].ids[0]), id); // get its id (specified in the config file)
-
+				if (objMap_.find(msgIn->markers[i].ids[0]) != objMap_.end()) { // if ID in map
 					field_obj::Object obj;
 
 					const auto &p = msgIn->markers[i].pose.position;
@@ -49,7 +70,7 @@ class FakeGoalDetection
 					obj.location.z = p.z;
 					obj.angle = atan2(obj.location.y, obj.location.x) * 180. / M_PI;
 					obj.confidence = msgIn->markers[i].ids_confidence[0];
-					obj.id = id;
+					obj.id = objMap_[msgIn->markers[i].ids[0]];
 					msgOut.objects.push_back(obj);
 				} else {
 					field_obj::Object dummy;
@@ -66,41 +87,6 @@ class FakeGoalDetection
 			}
 			pub_.publish(msgOut);
 			pubd_.publish(msgOut);
-			if (msgOut.objects.size() > 0)
-			{
-				//Transform between goal frame and odometry/map.
-				static tf2_ros::TransformBroadcaster br;
-				for(size_t i = 0; i < msgOut.objects.size(); i++)
-				{
-					if (msgOut.objects[i].id == "power_cell")
-					{
-						geometry_msgs::TransformStamped transformStamped;
-
-						transformStamped.header.stamp = msgOut.header.stamp;
-						transformStamped.header.frame_id = "base_fiducial_link";
-						std::stringstream child_frame;
-						child_frame << "power_cell_";
-						child_frame << i;
-						transformStamped.child_frame_id = child_frame.str();
-
-						transformStamped.transform.translation.x = msgOut.objects[i].location.x;
-						transformStamped.transform.translation.y = msgOut.objects[i].location.y;
-						transformStamped.transform.translation.z = msgOut.objects[i].location.z;
-
-						// Can't detect rotation yet, so publish 0 instead
-						tf2::Quaternion q;
-						q.setRPY(0, 0, 0);
-
-						transformStamped.transform.rotation.x = q.x();
-						transformStamped.transform.rotation.y = q.y();
-						transformStamped.transform.rotation.z = q.z();
-						transformStamped.transform.rotation.w = q.w();
-
-						br.sendTransform(transformStamped);
-					}
-				}
-
-			}
 		}
 
 	private:
@@ -108,10 +94,10 @@ class FakeGoalDetection
 		std::mt19937 gen_;
 		std::normal_distribution<double> normalDistribution_;
 		double covariance_;
-		ros::Subscriber       sub_;
-		ros::Publisher        pub_;
-		ros::Publisher       pubd_; // d for detection
-		ros::NodeHandle nh_params_;
+		ros::Subscriber            sub_;
+		ros::Publisher             pub_;
+		ros::Publisher             pubd_; // d for detection
+		std::map<int, std::string> objMap_;
 };
 
 int main(int argc, char** argv)
@@ -119,8 +105,7 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "fake_goal_detect");
 
 	ros::NodeHandle nh;
-	ros::NodeHandle nh_params(nh, "fake_goal_detect_params"); // node handle for a lower-down namespace
-	FakeGoalDetection fakeGoalDetection(nh, nh_params);
+	FakeGoalDetection fakeGoalDetection(nh, parsePBTXT("/home/ubuntu/2022RobotCode/zebROS_ws/src/tf_object_detection/src/2022Game_label_map.pbtxt"));
 
 	ros::spin();
 	return 0;
