d00a7b5838f359d780e87a6855980bcbe517b6ef
Kevin Jaget <kjaget@nc.rr.com>
2022-01-22 14:43:49 -0500

    Fix cross builds

--- zebROS_ws/rostoolchain.cmake
+++ zebROS_ws/rostoolchain.cmake
@@ -19,10 +19,6 @@ set(Boost_NO_SYSTEM_PATHS=ON)
 find_program(CMAKE_RANLIB ${ARM_PREFIX}-gcc-ranlib)
 find_program(CMAKE_AR ${ARM_PREFIX}-gcc-ar)
 
-if(NOT CMAKE_CXX_STANDARD)
-  set(CMAKE_CXX_STANDARD 17)
-endif()
-
 set(CMAKE_C_FLAGS_DEBUG "${C_FLAGS_DEBUG} -fPIC")
 set(CMAKE_CXX_FLAGS_DEBUG "${C_FLAGS_DEBUG} -fPIC")
 set(OPT_FLAGS "-O3 -flto=jobserver -fno-fat-lto-objects -fuse-linker-plugin -mcpu=cortex-a9 -mfpu=neon -fvect-cost-model -ffunction-sections -fdata-sections -Wl,-gc-sections -Wno-psabi")
