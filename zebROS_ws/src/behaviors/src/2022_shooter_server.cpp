c8c8a2605797dbeae6310bf575aef8863cf2175d
BenBean18 <beakerthespeaker@gmail.com>
2022-02-10 18:32:31 -0500

    Basic shooter action stuff works! side note: dynamic reconfigure is very fun to use :)

--- zebROS_ws/src/behaviors/src/2022_shooter_server.cpp
+++ zebROS_ws/src/behaviors/src/2022_shooter_server.cpp
@@ -20,8 +20,8 @@ protected:
   // create message that is used to publish feedback
   behavior_actions::Shooter2022Feedback feedback_;
 
-  DynamicReconfigureWrapper<Shooter2022Config> dynamic_reconfigure_server_;
-  Shooter2022Config config_;
+  DynamicReconfigureWrapper<behaviors::Shooter2022Config> dynamic_reconfigure_server_;
+  behaviors::Shooter2022Config config_;
 
   ros::Publisher shooter_command_pub_;
 
@@ -34,62 +34,74 @@ protected:
 public:
 
   ShooterAction2022(std::string name) :
-    as_(nh_, name, boost::bind(ShooterAction2022::executeCB, this, _1), false),
+    as_(nh_, name, boost::bind(&ShooterAction2022::executeCB, this, _1), false),
     action_name_(name)
   {
     if (!nh_.getParam("high_goal_speed", config_.high_goal_speed))
   	{
-  		SHOOTER_ERROR("Couldn't find param high_goal_speed. Aborting initialization.")
-      return false;
+  		SHOOTER_ERROR("Couldn't find param high_goal_speed. Aborting initialization.");
+      exit(EXIT_FAILURE);
   	}
     if (!nh_.getParam("low_goal_speed", config_.low_goal_speed))
   	{
-  		SHOOTER_ERROR("Couldn't find param low_goal_speed. Aborting initialization.")
-      return false;
+  		SHOOTER_ERROR("Couldn't find param low_goal_speed. Aborting initialization.");
+      exit(EXIT_FAILURE);
   	}
     if (!nh_.getParam("eject_speed", config_.eject_speed))
   	{
-  		SHOOTER_ERROR("Couldn't find param eject_speed. Aborting initialization.")
-      return false;
+  		SHOOTER_ERROR("Couldn't find param eject_speed. Aborting initialization.");
+      exit(EXIT_FAILURE);
   	}
     if (!nh_.getParam("error_margin", config_.error_margin))
   	{
-  		SHOOTER_ERROR("Couldn't find param error_margin. Aborting initialization.")
-      return false;
+  		SHOOTER_ERROR("Couldn't find param error_margin. Aborting initialization.");
+      exit(EXIT_FAILURE);
   	}
     shooter_command_pub_ = nh_.advertise<std_msgs::Float64>("/frcrobot_jetson/shooter_controller/command", 2);
     talon_states_sub_ = nh_.subscribe("/frcrobot_jetson/talon_states", 1, &ShooterAction2022::talonStateCallback, this);
     as_.start();
+
+    dynamic_reconfigure_server_.init(nh_, config_);
   }
 
-  ShooterAction2022(void)
+  ~ShooterAction2022(void)
   {
   }
 
   void executeCB(const behavior_actions::Shooter2022GoalConstPtr &goal)
   {
-    SHOOTER_INFO("Shooter action called with mode " << goal.mode);
+    SHOOTER_INFO("Shooter action called with mode " << std::to_string(goal->mode));
     ros::Rate r(100);
     while (ros::ok()) {
       ros::spinOnce();
-      switch (goal.mode) {
-        case behavior_actions::Shooter2022Action::Goal::HIGH_GOAL:
+      std_msgs::Float64 msg;
+      if (as_.isPreemptRequested() || !ros::ok())
+      {
+        msg.data = 0;
+        shooter_command_pub_.publish(msg);
+        ROS_INFO("%s: Preempted", action_name_.c_str());
+        // set the action state to preempted
+        as_.setPreempted();
+        break;
+      }
+      switch (goal->mode) {
+        case behavior_actions::Shooter2022Goal::HIGH_GOAL:
           feedback_.close_enough = fabs(config_.high_goal_speed - current_speed_) < config_.error_margin;
-          std_msgs::Float64 msg;
           msg.data = config_.high_goal_speed;
           shooter_command_pub_.publish(msg);
           break;
-        case behavior_actions::Shooter2022Action::Goal::LOW_GOAL:
+        case behavior_actions::Shooter2022Goal::LOW_GOAL:
           feedback_.close_enough = fabs(config_.low_goal_speed - current_speed_) < config_.error_margin;
           msg.data = config_.low_goal_speed;
           shooter_command_pub_.publish(msg);
           break;
-        case behavior_actions::Shooter2022Action::Goal::EJECT:
+        case behavior_actions::Shooter2022Goal::EJECT:
           feedback_.close_enough = fabs(config_.eject_speed - current_speed_) < config_.error_margin;
           msg.data = config_.eject_speed;
           shooter_command_pub_.publish(msg);
           break;
       }
+      as_.publishFeedback(feedback_);
       r.sleep();
     }
   }
