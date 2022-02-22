8baab9faf6a3029347c5b91e3e30591c9b3df143
Sahil Bhatia <sbhatia01@gmail.com>
2022-02-22 17:56:40 -0500

    Moved the fake_terabee code to the fake_sensors package, fixed the indentation, initialized the publisher and subscriber, and fixed the values being added to msgOut

--- zebROS_ws/src/fake_sensors/src/fake_terabee.cpp
+++ zebROS_ws/src/fake_sensors/src/fake_terabee.cpp
@@ -3,30 +3,33 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
-#include <sensor_msgs/Distance.h>
 #include <sensor_msgs/Range.h>
+#include <random>
 
 class FakeTerabeeDist
 {
   public:
     FakeTerabeeDist(ros::NodeHandle &n)
-                    : rd_{}
-                    , gen_{rd_()}           //generating a random digit
-                    , sub_(n.subscribe("fake_terabee_input", 2, &FakeTerabeeDist::cmdVelCallback, this))
-                    , pub_(n.advertise<sensor_msgs::Range>("terabee", 2))
+      : rd_{}
+      , gen_{rd_()}           //generating a random digit
+      , sub_(n.subscribe("fake_terabee_input", 2, &FakeTerabeeDist::cmdVelCallback, this))
+      , pub_(n.advertise<sensor_msgs::Range>("terabee", 2))
+
+    {
+
+    }
                     //covariance??
     void cmdVelCallback(const sensor_msgs::LaserScanConstPtr &msgIn)
     {
 			sensor_msgs::Range msgOut;
 			msgOut.header.stamp = msgIn->header.stamp;
-			msgOut.header.frame_id = "terabee";
-			msgOut.seq = msgIn->seq;
-      msgOut.stamp = msgIn->stamp;    //formatting same as above
-      msgOut.range = msgIn->ranges[0]+normalDistribution_(gen_)); //adding noise
+			msgOut.header.frame_id = msgIn->header.frame_id;
+			msgOut.header.seq = msgIn->header.seq;
+      msgOut.range = msgIn->ranges[0]+normalDistribution_(gen_); //adding noise
       msgOut.radiation_type = 1;
-      msgOut.range_min = msgIn->min_range; //check lines 27-29
-      msgOut.range_max = msgIn->max_range;
-      msgOut.field_of_view = msgIn->angle_max-angle_min
+      msgOut.min_range = msgIn->range_min; //check lines 27-29
+      msgOut.max_range = msgIn->range_max;
+      msgOut.field_of_view = msgIn->angle_max-msgIn->angle_min;
 
 			pub_.publish(msgOut);
     }
