f108c5b78dff3b6feb29902359bd3a0347a2f2fc
BenBean18 <beakerthespeaker@gmail.com>
2022-03-04 14:13:51 -0500

    Added indexer functionality. It compiles, but wasn't able to test yet due to an indexer server error.

--- zebROS_ws/src/behaviors/src/2022_intaking_server.cpp
+++ zebROS_ws/src/behaviors/src/2022_intaking_server.cpp
@@ -3,7 +3,8 @@
 #include <actionlib/client/simple_action_client.h>
 #include <behavior_actions/Intaking2022Action.h>
 #include <behavior_actions/Intake2022Action.h>
-// #include <behavior_actions/Indexer2022Action.h>
+#include <behavior_actions/Index2022Action.h>
+#include <std_msgs/Float64.h>
 
 class IntakingServer2022
 {
@@ -17,18 +18,22 @@ protected:
 	behavior_actions::Intaking2022Result result_;
 
 	actionlib::SimpleActionClient<behavior_actions::Intake2022Action> intake_ac_;
-	// actionlib::SimpleActionClient<behavior_actions::Indexer2022Action> indexer_ac_;
+	actionlib::SimpleActionClient<behavior_actions::Index2022Action> indexer_ac_;
 
 	double server_timeout_;
+
+	ros::Subscriber indexer_cargo_num_;
+	uint8_t cargo_num_;
 
 public:
 
 	IntakingServer2022(std::string name) :
 		as_(nh_, name, boost::bind(&IntakingServer2022::executeCB, this, _1), false),
 		action_name_(name),
-		intake_ac_("/intake/intake_server_2022", true)/*,
-		indexer_ac_("/insert_path_here", true)*/
+		intake_ac_("/intake/intake_server_2022", true),
+		indexer_ac_("/index/index_server_2022", true)
 	{
+		indexer_cargo_num_ = nh_.subscribe("/2022_index_server/ball_state", 5, &IntakingServer2022::updateCargoNum, this);
 		if (!nh_.getParam("server_timeout", server_timeout_)) {
 			ROS_WARN_STREAM("2022_intaking_server : could not find server_timeout, defaulting to 10 seconds");
 			server_timeout_ = 10;
@@ -40,19 +45,34 @@ public:
 	{
 	}
 
+	void updateCargoNum(const std_msgs::Float64::ConstPtr& msg) {
+		cargo_num_ = msg->data;
+	}
+
 	void executeCB(const behavior_actions::Intaking2022GoalConstPtr &goal)
 	{
-		// If the requested number of cargo is more than we can intake (using indexer data) or is 0, abort.
+		ros::spinOnce();
+		if (goal->num_cargo + cargo_num_ > 2) {
+			ROS_ERROR_STREAM("2022_intaking_server : can't intake that many cargo! aborting!");
+			result_.correct_cargo = false;
+			as_.setAborted(result_);
+		}
 
 		if(!intake_ac_.waitForServer(ros::Duration(server_timeout_)))
 		{
 			ROS_ERROR_STREAM("2022_intaking_server : intake actionlib server does not exist. exiting.");
-			// TODO set result_.correct_cargo based on indexer data
+			result_.correct_cargo = false;
 			as_.setAborted(result_);
 			return;
 		}
 
-		// Do the above for the indexer client
+		if(!indexer_ac_.waitForServer(ros::Duration(server_timeout_)))
+		{
+			ROS_ERROR_STREAM("2022_intaking_server : indexer actionlib server does not exist. exiting.");
+			result_.correct_cargo = false;
+			as_.setAborted(result_);
+			return;
+		}
 
 		ROS_INFO_STREAM("2022_intaking_server : activating intake");
 		behavior_actions::Intake2022Goal intake_goal;
@@ -60,7 +80,20 @@ public:
 		intake_goal.reverse = false;
 		intake_ac_.sendGoal(intake_goal);
 
-		// Do indexer stuff until we intake enough cargo.
+		while (cargo_num_ < goal->num_cargo) {
+			ros::spinOnce();
+			behavior_actions::Index2022Goal index_goal;
+			index_goal.goal = index_goal.INTAKE;
+			indexer_ac_.sendGoal(index_goal);
+			bool finished_before_timeout = indexer_ac_.waitForResult(ros::Duration(server_timeout_));
+			ros::spinOnce();
+			result_.correct_cargo = cargo_num_ == goal->num_cargo;
+			feedback_.cargo_in_indexer = cargo_num_;
+			if (!finished_before_timeout) {
+				as_.setAborted(result_);
+			}
+			ros::spinOnce();
+		}
 
 		ROS_INFO_STREAM("2022_intaking_server : deactivating intake");
 		intake_ac_.cancelGoal();
