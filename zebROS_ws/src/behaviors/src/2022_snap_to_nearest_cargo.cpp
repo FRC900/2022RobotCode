72027444b596328d748fa2e1af6f3e041d610ba0
BenBean18 <beakerthespeaker@gmail.com>
2022-03-23 21:20:32 -0400

    Initial cargo snapping, seems to somewhat work but stage is being weird

---
+++ zebROS_ws/src/behaviors/src/2022_snap_to_nearest_cargo.cpp
@@ -0,0 +1,88 @@
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <behavior_actions/SnapToCargo2022Action.h>
+#include <field_obj/Detection.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Float64.h>
+
+class SnapToCargo2022Action
+{
+protected:
+
+  ros::NodeHandle nh_;
+  actionlib::SimpleActionServer<behavior_actions::SnapToCargo2022Action> as_;
+  std::string action_name_;
+  ros::Publisher orient_strafing_enable_pub_;
+  ros::Publisher orient_strafing_setpoint_pub_;
+  ros::Publisher orient_strafing_state_pub_;
+  ros::Subscriber object_detection_sub_;
+  double nearest_cargo_angle_;
+  bool find_opponent_cargo_;
+
+public:
+
+  SnapToCargo2022Action(std::string name) :
+    as_(nh_, name, boost::bind(&SnapToCargo2022Action::executeCB, this, _1), false),
+    action_name_(name)
+  {
+    as_.start();
+    orient_strafing_enable_pub_ = nh_.advertise<std_msgs::Bool>("/teleop/orient_strafing/pid_enable", 1);
+  	orient_strafing_setpoint_pub_ = nh_.advertise<std_msgs::Float64>("/teleop/orient_strafing/setpoint", 1);
+  	orient_strafing_state_pub_ = nh_.advertise<std_msgs::Float64>("/teleop/orient_strafing/state", 1);
+    object_detection_sub_ = nh_.subscribe("/tf_object_detection/object_detection_world_filtered", 1, &SnapToCargo2022Action::objectDetectionCallback, this);
+  }
+
+  void objectDetectionCallback(const field_obj::Detection &msg) {
+    field_obj::Object closest_cargo;
+    double shortest_distance = std::numeric_limits<double>::max();
+    for (auto obj : msg.objects) {
+      if (obj.id == (find_opponent_cargo_ ? "opponent_cargo" : "friendly_cargo")) {
+        double dist = hypot(obj.location.x, obj.location.y);
+        if (dist < shortest_distance) {
+          closest_cargo = obj;
+          shortest_distance = dist;
+        }
+      }
+    }
+    nearest_cargo_angle_ = closest_cargo.angle;
+  }
+
+  ~SnapToCargo2022Action(void)
+  {
+  }
+
+  void executeCB(const behavior_actions::SnapToCargo2022GoalConstPtr &goal)
+  {
+    find_opponent_cargo_ = goal->find_opponent_cargo;
+    std_msgs::Bool enable_align_msg;
+    enable_align_msg.data = true;
+    orient_strafing_enable_pub_.publish(enable_align_msg);
+    std_msgs::Float64 orient_strafing_angle_msg;
+    ros::Rate r(100);
+    ROS_INFO_STREAM("2022_snap_to_cargo : snapping to cargo");
+    while (!as_.isPreemptRequested() && ros::ok()) {
+      orient_strafing_enable_pub_.publish(enable_align_msg);
+      orient_strafing_angle_msg.data = nearest_cargo_angle_ * (M_PI/180.0);
+      orient_strafing_setpoint_pub_.publish(orient_strafing_angle_msg);
+      ROS_INFO_STREAM_THROTTLE(0.25, "2022_snap_to_cargo : currently snapped to the nearest cargo (angle: " << orient_strafing_angle_msg.data << " rad)");
+      r.sleep();
+      ros::spinOnce();
+    }
+    ROS_INFO_STREAM("2022_snap_to_cargo : motion freed");
+    enable_align_msg.data = false;
+    orient_strafing_enable_pub_.publish(enable_align_msg);
+  }
+
+
+};
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "snap_to_cargo_2022");
+
+  SnapToCargo2022Action action("snap_to_cargo_2022");
+  ros::spin();
+
+  return 0;
+}
