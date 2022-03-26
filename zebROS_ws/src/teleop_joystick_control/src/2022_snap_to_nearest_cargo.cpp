848c483e8b47a0c0f0f1bcf8afc21ff814b186fb
BenBean18 <beakerthespeaker@gmail.com>
2022-03-26 11:32:13 -0400

    Now transforms to base_link

--- zebROS_ws/src/teleop_joystick_control/src/2022_snap_to_nearest_cargo.cpp
+++ zebROS_ws/src/teleop_joystick_control/src/2022_snap_to_nearest_cargo.cpp
@@ -4,6 +4,10 @@
 #include <field_obj/Detection.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/Float64.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/QuaternionStamped.h>
 
 class SnapToCargo2022
 {
@@ -15,8 +19,14 @@ protected:
   ros::Publisher nearest_opponent_cargo_pub_;
   double nearest_cargo_angle_;
   double nearest_opponent_cargo_angle_;
+  tf2_ros::Buffer tf_buffer_;
+  tf2_ros::TransformListener tf_listener_;
 
 public:
+
+  SnapToCargo2022() : tf_listener_(tf_buffer_) {
+
+  }
 
   void init() {
     nearest_cargo_pub_ = nh_.advertise<std_msgs::Float64>("nearest_cargo_angle", 1);
@@ -46,12 +56,46 @@ public:
       }
     }
     std_msgs::Float64 msg1;
-    msg1.data = closest_cargo.angle * (M_PI/180.0);
+    try {
+      geometry_msgs::QuaternionStamped q1s;
+      tf2::Quaternion q1;
+      q1.setRPY(0, 0, closest_cargo.angle * (M_PI/180.0));
+      q1s.header = msg.header;
+      q1s.quaternion.x = q1.x();
+      q1s.quaternion.y = q1.y();
+      q1s.quaternion.z = q1.z();
+      q1s.quaternion.w = q1.w();
+      q1s = tf_buffer_.transform(q1s, "base_link", ros::Duration(0.05));
+      double r1, p1, y1;
+      tf2::Matrix3x3 q1_m(q1);
+      q1_m.getRPY(r1, p1, y1);
+      msg1.data = y1;
+    } catch (...) {
+      ROS_WARN_STREAM_THROTTLE(0.1, "snap_to_cargo_2022 : transform to base_link failed, using untransformed angle");
+      msg1.data = closest_cargo.angle * (M_PI/180.0);
+    }
     nearest_cargo_pub_.publish(msg1);
+
     std_msgs::Float64 msg2;
-    msg2.data = closest_opponent_cargo.angle * (M_PI/180.0);
+    try {
+      geometry_msgs::QuaternionStamped q2s;
+      tf2::Quaternion q2;
+      q2.setRPY(0, 0, closest_opponent_cargo.angle * (M_PI/180.0));
+      q2s.header = msg.header;
+      q2s.quaternion.x = q2.x();
+      q2s.quaternion.y = q2.y();
+      q2s.quaternion.z = q2.z();
+      q2s.quaternion.w = q2.w();
+      q2s = tf_buffer_.transform(q2s, "base_link", ros::Duration(0.05));
+      double r2, p2, y2;
+      tf2::Matrix3x3 q2_m(q2);
+      q2_m.getRPY(r2, p2, y2);
+      msg2.data = y2;
+    } catch (...) {
+      ROS_WARN_STREAM_THROTTLE(0.1, "snap_to_cargo_2022 : transform to base_link failed, using untransformed angle");
+      msg2.data = closest_opponent_cargo.angle * (M_PI/180.0);
+    }
     nearest_opponent_cargo_pub_.publish(msg2);
-    ROS_INFO_STREAM_THROTTLE(1, "snap_to_cargo_2022 : publishing");
   }
 
 
