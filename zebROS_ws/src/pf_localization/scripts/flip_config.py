667fbe5d6c5b9716cc05fa93b86ecd5536c7e493
Ubuntu <ubuntu@nv-ncs1-app010.apporto.com>
2022-10-13 13:11:17 -0400

    add script for changing tags based on new 0,0

---
+++ zebROS_ws/src/pf_localization/scripts/flip_config.py
@@ -0,0 +1,22 @@
+import yaml
+import path
+config_root = "/home/ubuntu/2022RobotCode/zebROS_ws/src/pf_localization/config"
+
+with open(config_root + "/2022Rumble_params.yaml") as file_:
+    data = yaml.load(file_, Loader=yaml.Loader)
+print(data)
+print("\n")
+
+# Define where the new 0,0 should be
+new_x = 16.458
+new_y = 0
+beacons = data["beacons"]
+for beacon in beacons: 
+    beacon[0] = round(beacon[0] - new_x, 3)
+    beacon[1] = round(beacon[1] - new_y, 3)
+    
+
+data["beacons"] = beacons
+print(data)
+with open(config_root + "/2022Rumble_params_flipped.yaml", 'w') as file:
+    documents = yaml.dump(data, file)
