d3e84901b4ac560f1a315fc375c9195ff8b4d024
Torchtopher <torchtopher@gmail.com>
2022-10-16 12:37:55 -0400

    Add blue->red beacon transform and dynamic beacon selection based on alliance color

--- zebROS_ws/src/pf_localization/include/pf_localization/world_model.hpp
+++ zebROS_ws/src/pf_localization/include/pf_localization/world_model.hpp
@@ -10,6 +10,9 @@
 class WorldModel {
 private:
   std::vector<PositionBeacon> beacons_;
+  const std::vector<PositionBeacon> blue_beacons_;
+  const std::vector<PositionBeacon> red_beacons_;
+  bool amIBlueAlliance_ = false;
   double x_min_;
   double x_max_;
   double y_min_;
@@ -17,15 +20,17 @@ private:
   AssignmentProblemSolver solver_;
   std::vector<PositionBeacon> of_type(const std::vector<PositionBeacon>& bcns, const std::string &type);
   std::vector<PositionBeacon> single_particle_relative(const Particle& p, const std::vector<PositionBeacon> &bcns, const std::string &type) const;
+  
 public:
   std::vector<PositionBeacon> particle_relative(const Particle& p) const;
-  WorldModel(const std::vector<PositionBeacon>& beacons,
+  WorldModel(std::vector<PositionBeacon>& beacons, const std::vector<PositionBeacon>& blue_beacons, const std::vector<PositionBeacon>& red_beacons,
              double x_min, double x_max, double y_min, double y_max);
   void get_boundaries(double &x_min, double &x_max, double &y_min, double &y_max) const;
   //bool is_in_world(const Particle& p) const;
   void constrain_to_world(Particle& p) const;
   void init_particle(Particle& p, double x_min, double x_max, double y_min, double y_max);
   double total_distance(const Particle& p, const std::vector<std::shared_ptr<BeaconBase>>& m) const;
+  bool allianceColorCheck(bool amIBlueAlliance);
 };
 
 #endif
