9bc995f801a8cc702aa293b4fb296a60583f6187
Kevin Jaget <kjaget@nc.rr.com>
2022-12-27 17:52:53 -0500

    Lots of updates to get bearing-only PF in shape
     - viz updates to handle multiple beacons of a given type
     - simplify beacon<->measurement assignment (run 1 instance of
    	optimal assignment for all types rather than 1 per type
     - update bearing-only distance score
     - filter out beacons which couldn't be seen by a particle when
            doing beacon<->measurement assignments
     - simple script to log all detections to file

--- zebROS_ws/src/pf_localization/scripts/pf_vis_bak.py
+++ zebROS_ws/src/pf_localization/scripts/pf_vis_bak.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-import numpy as np
+from dataclasses import dataclass, field
 import math
 import matplotlib.pyplot as plt
-from threading import Lock
+#from typing import list
+import matplotlib
+matplotlib.use('TkAgg')
 
 import rospy
 from nav_msgs.msg import Odometry
@@ -57,103 +59,71 @@ class RingBuffer:
         return self.data[-1]
 
 max_pose_points = 30
-predicted_x = RingBuffer(max_pose_points)
-predicted_y = RingBuffer(max_pose_points)
-ground_truth_x = RingBuffer(max_pose_points)
-ground_truth_y = RingBuffer(max_pose_points)
-beacon_locations = {}
-beacon_locations_blue = {}
-beacon_locations_red = {}
-beacon_annotations = []
+@dataclass
+class PlotData:
+    # History of predicted x&y values, plus current predicted angle
+    predicted_x: RingBuffer(max_pose_points) = RingBuffer(max_pose_points)
+    predicted_y: RingBuffer(max_pose_points) = RingBuffer(max_pose_points)
+    angle: float = 0.0
 
-plt_mutex = Lock()
-current_alliance_color = 0
+    # Most recent set of particle x & y values
+    particle_x: list[float] = field(default_factory=list)
+    particle_y: list[float] = field(default_factory=list)
 
-fig, axes = plt.subplots(nrows=1, ncols=1)
-ax = axes
-pln, = ax.plot([], [], 'r.', markersize=2)
-dir, = ax.plot([], [], 'r-,')
-ln,  = ax.plot([], [], 'b.', markersize=.15)
-gtn, = ax.plot([], [], 'g.', markersize=2)
-dtns = []
+    # Most recent set of x,y beacons seen
+    beacon_x: list[float] = field(default_factory=list)
+    beacon_y: list[float] = field(default_factory=list)
 
-def update_debug(debug_msg):
-    global ln, pln, dir, dtns, predicted_x, predicted_y, beacon_locations
+    # Ground truth from sim
+    ground_truth_x: RingBuffer = RingBuffer(max_pose_points)
+    ground_truth_y: RingBuffer = RingBuffer(max_pose_points)
 
-    x_data = []
-    y_data = []
-    rot_data = []
+plot_data = PlotData()
+
+def update_debug(debug_msg : PFDebug):
+    global plot_data
+
+    #rospy.loginfo("update_debug")
+    #rot_data = []
+    plot_data.particle_x.clear()
+    plot_data.particle_y.clear()
     for p in debug_msg.poses:
-        x_data.append(p.position.x)
-        y_data.append(p.position.y)
-        angles = euler_from_quaternion([p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w])
-        rot_data.append(angles[2])
+        plot_data.particle_x.append(p.position.x)
+        plot_data.particle_y.append(p.position.y)
+    #rospy.logwarn(plot_data.particle_x)
+        #angles = euler_from_quaternion([p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w]);
+        #rot_data.append(angles[2])
 
-    this_x = debug_msg.predicted_pose.position.x
-    this_y = debug_msg.predicted_pose.position.y
-    predicted_x.append(this_x)
-    predicted_y.append(this_y)
+    plot_data.predicted_x.append(debug_msg.predicted_pose.position.x)
+    plot_data.predicted_y.append(debug_msg.predicted_pose.position.y)
 
     o = debug_msg.predicted_pose.orientation
-    angle = euler_from_quaternion([o.x, o.y, o.z, o.w])[2]
+    plot_data.angle = euler_from_quaternion([o.x, o.y, o.z, o.w])[2]
 
-    plt_mutex.acquire()
-    for i in range(len(dtns)) :
-        if i < len(debug_msg.beacons):
-            xs = [predicted_x.newest(), debug_msg.beacons[i].position.x]
-            ys = [predicted_y.newest(), debug_msg.beacons[i].position.y]
-            dtns[i].set_data(xs,ys)
-        else:
-            dtns[i].set_data([], [])
+    plot_data.beacon_x.clear()
+    plot_data.beacon_y.clear()
+    for b in debug_msg.beacons:
+        plot_data.beacon_x.append(b.position.x)
+        plot_data.beacon_y.append(b.position.y)
 
-    ln.set_data(x_data, y_data)
-    pln.set_data(predicted_x.get(), predicted_y.get())
-    dir.set_data([this_x, this_x + math.cos(angle)],[this_y, this_y + math.sin(angle)])
-    plt.draw()
-    plt_mutex.release()
 
 def update_ground_truth(ground_truth_msg):
-    global ground_truth_x, ground_truth_y
+    global plot_data
 
-    ground_truth_x.append(ground_truth_msg.pose.pose.position.x)
-    ground_truth_y.append(ground_truth_msg.pose.pose.position.y)
-    plt_mutex.acquire()
-    gtn.set_data(ground_truth_x.get(), ground_truth_y.get())
-    plt.draw()
-    plt_mutex.release()
+    plot_data.ground_truth_x.append(ground_truth_msg.pose.pose.position.x)
+    plot_data.ground_truth_y.append(ground_truth_msg.pose.pose.position.y)
+
 
 def update_match_data(match_data_msg):
-    global beacon_locations, beacon_locations_blue, beacon_locations_red, current_alliance_color, ax
-    ac = match_data_msg.allianceColor
-    if (ac != current_alliance_color):
-        current_alliance_color = ac
-        if match_data_msg.allianceColor == 0:
-            beacon_locations = beacon_locations_red
-        else:
-            beacon_locations = beacon_locations_blue
-
-        plt_mutex.acquire()
-        for i, a in enumerate(beacon_annotations):
-            a.remove()
-        beacon_annotations[:] = []
-        
-        for id, ps in beacon_locations.items():
-            for p in ps:
-                beacon_annotations.append(ax.annotate(id, (p[0], p[1]), textcoords="offset points", xytext=(0,10), ha='center'))
-
-        plt.draw()
-        plt_mutex.release()
+    global alliance_color
+    alliance_color = match_data_msg.allianceColor
 
 
 def main():
-    global ax
-
-    rospy.init_node('pf_vis_node', anonymous = True)
+    rospy.init_node('pf_vis_node')
     rospy.loginfo("pf visualization node initialized")
 
     beacons = []
-    beacon_x = []
-    beacon_y = []
     field_dims = []
 
     if rospy.has_param('beacons'):
@@ -173,10 +143,19 @@ def main():
 
     rospy.loginfo("assigned field dimensions")
 
+    fig, axes = plt.subplots(1,1)
+    ax = axes[0]
+    position_plot, = ax.plot([], [], 'r.', markersize=2)
+    dir_plot, = ax.plot([], [], 'r-,')
+    particles_plot,  = ax.plot([], [], 'b.', markersize=.15)
+    ground_truth_plot, = ax.plot([], [], 'g.', markersize=2)
     ax.set_xlim(field_dims[0], field_dims[1])
     ax.set_ylim(field_dims[2], field_dims[3])
 
-    global beacon_locations, beacon_locations_blue, beacon_locations_red, current_alliance_color, dtns
+    dtns = []
+    beacon_x = []
+    beacon_y = []
+    beacon_locations_blue = {}
     for b in beacons:
         beacon_x.append(b[0])
         beacon_y.append(b[1])
@@ -186,8 +165,8 @@ def main():
         beacon_locations_blue[b[2]].append([b[0], b[1]])
 
         # And a placeholder for each potential predicted_pose->beacon line
-        dtn, = ax.plot([], [], 'r-')
-        dtns.append(dtn)
+        #dtn, = ax.plot([], [], 'r-')
+        #dtns.append(dtn)
 
     # Create mirror image beacon positions for robot
     # on red alliance
@@ -200,6 +179,7 @@ def main():
     q = quaternion_from_euler(0, 0, math.pi)
     trans.transform.rotation = Quaternion(*q)
 
+    beacon_locations_red = {}
     for k, v in beacon_locations_blue.items():
         beacon_locations_red[k] = []
         for vv in v:
@@ -210,24 +190,47 @@ def main():
             pose_t = tf2_geometry_msgs.do_transform_pose(pose_s, trans)
             beacon_locations_red[k].append([pose_t.pose.position.x, pose_t.pose.position.y])
 
-    current_alliance_color = 1
-    beacon_locations = beacon_locations_blue
-
-    ax.plot(beacon_x, beacon_y, 'ko')
-    for id, ps in beacon_locations.items():
-        for p in ps:
-            beacon_annotations.append(ax.annotate(id, (p[0], p[1]), textcoords="offset points", xytext=(0,10), ha='center'))
-
     sub_debug = rospy.Subscriber(debug_topic, PFDebug, update_debug)
     sub_ground_truth = rospy.Subscriber(ground_truth_topic, Odometry, update_ground_truth)
     sub_match_data = rospy.Subscriber(match_data_topic, MatchSpecificData, update_match_data)
 
     plt.show()
+    global plot_data
+    global alliance_color
+    alliance_color = 1
+    current_alliance_color = -1
+    beacon_annotations = []
+    r = rospy.Rate(21)
+    while not rospy.is_shutdown():
+        r.sleep()
 
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
+        rospy.loginfo(f"px = {plot_data.particle_x}")
+
+        if (alliance_color != current_alliance_color):
+            current_alliance_color = alliance_color
+            if alliance_color == 0:
+                beacon_locations = beacon_locations_red
+            else:
+                beacon_locations = beacon_locations_blue
+
+            ax.plot(beacon_x, beacon_y, 'ko')
+            for i, a in enumerate(beacon_annotations):
+                a.remove()
+            beacon_annotations[:] = []
+            
+            for id, ps in beacon_locations.items():
+                for p in ps:
+                    beacon_annotations.append(ax.annotate(id, (p[0], p[1]), textcoords="offset points", xytext=(0,10), ha='center'))
+        particles_plot.set_data(plot_data.particle_x, plot_data.particle_y)
+        position_plot.set_data(plot_data.predicted_x.get(), plot_data.predicted_y.get())
+        this_x = plot_data.predicted_x.newest()
+        this_y = plot_data.predicted_y.newest()
+        if (this_x is not None) and (this_y is not None):
+            dir_plot.set_data([this_x, this_x + math.cos(plot_data.angle)],[this_y, this_y + math.sin(plot_data.angle)])
+        ground_truth_plot.set_data(plot_data.ground_truth_x.get(), plot_data.ground_truth_y.get())
+
+        plt.draw()
+
 
 if __name__ == '__main__':
     main()
