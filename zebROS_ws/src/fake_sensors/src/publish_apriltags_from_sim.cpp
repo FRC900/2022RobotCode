5512ab6265caf28bae653983405935fd2cc9bcd9
BenBean18 <beakerthespeaker@gmail.com>
2023-01-12 20:55:22 -0500

    Update apriltag simulation publishing

--- zebROS_ws/src/fake_sensors/src/publish_apriltags_from_sim.cpp
+++ zebROS_ws/src/fake_sensors/src/publish_apriltags_from_sim.cpp
@@ -1,6 +1,7 @@
 // This node republishes fiducial markers corresponding to apriltags as the same
 // transforms that the apriltag detector sends
 // (we aren't using a camera in sim so we can't publish an AprilTagDetectionArray)
+// ... why can't we again?
 #include <random>
 #include <ros/ros.h>
 #include <marker_msgs/MarkerDetection.h>
@@ -9,6 +10,43 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
+#include <apriltag_ros/AprilTagDetectionArray.h>
+#include <apriltag_ros/AprilTagDetection.h>
+#include <std_msgs/Header.h>
+
+// topic: cuda_tag_detections
+/*
+apriltag_ros/AprilTagDetection[] detections
+  int32[] id
+  float64[] size
+  geometry_msgs/PoseWithCovarianceStamped pose
+    std_msgs/Header header
+      uint32 seq
+      time stamp
+      string frame_id
+    geometry_msgs/PoseWithCovariance pose
+      geometry_msgs/Pose pose
+        geometry_msgs/Point position
+          float64 x
+          float64 y
+          float64 z
+        geometry_msgs/Quaternion orientation
+          float64 x
+          float64 y
+          float64 z
+          float64 w
+      float64[36] covariance
+*/
+
+apriltag_ros::AprilTagDetection createAprilTag(uint32_t id, double x, double y, double z, const std_msgs::Header &header) {
+	apriltag_ros::AprilTagDetection msg;
+	msg.id = {id};
+	msg.pose.pose.pose.position.x = x;
+	msg.pose.pose.pose.position.y = y;
+	msg.pose.pose.pose.position.z = z;
+	msg.pose.header = header;
+	return msg;
+}
 
 #include <map>
 #include <regex>
@@ -37,7 +75,8 @@ class SimAprilTagPub
 {
 	public:
 		SimAprilTagPub(ros::NodeHandle &n)
-			: sub_(n.subscribe("/base_marker_detection", 2, &SimAprilTagPub::msgCallback, this))
+			: sub_(n.subscribe("/base_marker_detection", 2, &SimAprilTagPub::msgCallback, this)),
+			  pub_(n.advertise<apriltag_ros::AprilTagDetectionArray>("/cuda_tag_detections", 1000))
 
 		{
 			ROS_INFO_STREAM("Hi");
@@ -56,7 +95,8 @@ class SimAprilTagPub
 		{
 			static tf2_ros::TransformBroadcaster br;
 			static tf2_ros::Buffer tfBuffer;
-  		static tf2_ros::TransformListener tfListener(tfBuffer);
+  			static tf2_ros::TransformListener tfListener(tfBuffer);
+			std::vector<apriltag_ros::AprilTagDetection> detections;
 			for(size_t i = 0; i < msgIn->markers.size(); i++)
 			{
 				if (msgIn->markers[i].ids[0] == -1) // stage publishes odom as marker -1
@@ -86,7 +126,16 @@ class SimAprilTagPub
 				transformStamped.transform.rotation.w = q.w();
 
 				br.sendTransform(transformStamped);
+
+				detections.push_back(createAprilTag(msgIn->markers[i].ids[0]-100, p.x, p.y, tags_[msgIn->markers[i].ids[0]-100].z, msgIn->header));
+
 			}
+
+			apriltag_ros::AprilTagDetectionArray msg;
+			msg.detections = detections;
+			msg.header = msgIn->header;
+
+			pub_.publish(msg);
 			// pub_.publish(msgOut);
 			// pubd_.publish(msgOut);
 		}
@@ -94,6 +143,7 @@ class SimAprilTagPub
 	private:
 		ros::Subscriber            sub_;
 		std::map<int, Tag>         tags_; // tag id to tag info
+		ros::Publisher             pub_;
 };
 
 int main(int argc, char** argv)
